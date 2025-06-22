91180d682189a0267c20ac10d699a01e6a8526db
armalam <arman.u.alam@gmail.com>
2025-06-22 10:20:57 +0600

    feat: add book

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, ILike } from 'typeorm';
+import { Book } from './book.entity';
+import { Author } from '../authors/author.entity';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private bookRepo: Repository<Book>,
+
+    @InjectRepository(Author)
+    private authorRepo: Repository<Author>,
+  ) {}
+
+  async create(dto: CreateBookDto): Promise<Book> {
+    const author = await this.authorRepo.findOne({
+      where: { id: dto.authorId },
+    });
+    if (!author) {
+      throw new BadRequestException('Author does not exist');
+    }
+
+    const existing = await this.bookRepo.findOne({ where: { isbn: dto.isbn } });
+    if (existing) {
+      throw new ConflictException('ISBN already exists');
+    }
+
+    const book = this.bookRepo.create({ ...dto, author });
+    return this.bookRepo.save(book);
+  }
+}
